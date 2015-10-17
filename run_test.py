692f071d3803c601f5ef6c0a788a79d8668b1234
levelfour <levelfourslv@gmail.com>
2015-10-17 15:08:31 +0900

    fix test script

--- run_test.py
+++ run_test.py
@@ -2,10 +2,11 @@
 # coding: utf-8
 
 from assembler.assembler import Assembler
-from simulator.main import simulate
+from simulator.simulator import Simulator
 
 def test_recfib13():
 	# calculate recursive-fib(13)
 	asmblr = Assembler()
 	asmblr.assemble("tests/fib.s")
-	assert simulate("tests/fib.o") == 233
+	sim = Simulator("tests/fib.o")
+	assert sim.simulate() == 233
