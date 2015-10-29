48242424490724507a32db4e2668b0fff8e3f7ae
Yuki Imai <yuki08281993@gmail.com>
2015-10-30 00:29:24 +0900

    kusatta indent fixed

--- core/cpu.vhd
+++ core/cpu.vhd
@@ -152,56 +152,56 @@ begin
     variable controlv:control_file;
     variable vPC:datat;
   begin
-  if rising_edge(clk) then  
-  case (core_state) is
-    when INIT=>
-      loader_reset<='0';
-      if io_full='0' then
-        io_we<='1';
-        io_send_data<=x"4341524e";
-      else
-        io_we<='0';
-        core_state<=WaIT_HEADER;
-      end if;
-    when WAIT_HEADER =>
+    if rising_edge(clk) then  
+      case (core_state) is
+        when INIT=>
+          loader_reset<='0';
+          if io_full='0' then
+            io_we<='1';
+            io_send_data<=x"4341524e";
+          else
+            io_we<='0';
+            core_state<=WaIT_HEADER;
+          end if;
+        when WAIT_HEADER =>
       --debug
-      DEBUG.data1<=inst_out;
-      DEBUG.core_state<=core_state;
-      DEBUG.PC<=CONV_STD_LOGIC_VECTOR(count,32);
-      DEBUG.control<=control;
+          DEBUG.data1<=inst_out;
+          DEBUG.core_state<=core_state;
+          DEBUG.PC<=CONV_STD_LOGIC_VECTOR(count,32);
+          DEBUG.control<=control;
       --/debug
-      loader_activate<='1';
-      if loaded='1' then
+          loader_activate<='1';
+          if loaded='1' then
 			io_send_data<=x"52435644";
 			IO_WE<='1';
             PC<=entry_point;
-        core_state<=EXE_READY;
---      else
+            core_state<=EXE_READY;
+--       else
 --        io_send_data<=io_recv_data;
 --        io_we<=loader_io_re;
-      end if;
-    when EXE_READY=>
-			io_we<='0';
-      report "exe ready";
-      core_state<=EXECUTING;    --data source no kirikae
-    when EXECUTING =>
-  --debug
-  DEBUG.opecode<=inst.opecode;
-  DEBUG.control<=control;
-  
-  DEBUG.data<=data;
-  DEBUG.data1<=reg_file(1);
-  DEBUG.data2<=reg_file(2);
-  DEBUG.exe_state<=exe_state;
-  DEBUG.core_state<=core_state;
-  DEBUG.PC<=PC;
-  DEBUG.inst<=inst;
-  DEBUG.data3<=reg_file(3);
-  DEBUG.alucont<=alu_control;
+          end if;
+        when EXE_READY=>
+          io_we<='0';
+          report "exe ready";
+          core_state<=EXECUTING;    --data source no kirikae
+        when EXECUTING =>
+      --debug
+          DEBUG.opecode<=inst.opecode;
+          DEBUG.control<=control;
+
+          DEBUG.data<=data;
+          DEBUG.data1<=reg_file(1);
+          DEBUG.data2<=reg_file(2);
+          DEBUG.exe_state<=exe_state;
+          DEBUG.core_state<=core_state;
+          DEBUG.PC<=PC;
+          DEBUG.inst<=inst;
+          DEBUG.data3<=reg_file(3);
+          DEBUG.alucont<=alu_control;
   --/debug
-      
-      case ( exe_state) is
-        when F =>
+
+          case ( exe_state) is
+            when F =>
 --		    report "inst_out:" & integer'image(conv_integer(inst_out));
 --			 report "PC:" &  integer'image(conv_integer(PC));
 -- ###################################DEBUG############################
@@ -214,150 +214,145 @@ begin
 --        when D =>
 --				io_we<='0';
 --#####################################################################
-			exe_state<=D;
-		 when D=>
-			 instv.PC:=PC;
-          instv.instruction:=inst_out;
-          instv.opecode:= inst_out(31 downto 26);
-          instv.rs:=inst_out(25 downto 21);
-          instv.rt:=inst_out(20 downto 16);
-          instv.rd:=inst_out(15 downto 11);
-          instv.shamt:=inst_out(10 downto 6);
-          instv.funct:=inst_out(5 downto 0);
-          instv.immediate:=inst_out(15 downto 0);
-          instv.addr:=inst_out(25 downto 0); 
-          exe_state<=EX;
-			 if inst_out=x"FFFFFFFF" then
---				core_state<=HALTED;
- 			core_state<=INIT;
-            loader_reset<='1';
-            exe_state<=F;
-			end if;
-			 
-          controlv:=make_control(instv.opecode,instv.funct);
-          if controlv.RegDst='0' then
-            instv.reg_dest:=instv.rd;
-          else
-            instv.reg_dest:=instv.rt;
-          end if;
-          inst<=instv;
-          data.operand1<=reg_file(CONV_INTEGER(instv.rs));
-          if controlv.ALUSrc='0' then
-            data.operand2<=reg_file(CONV_INTEGER(instv.rt));
-          else
-            if instv.immediate(15) ='0' then
-              data.operand2<="0000000000000000" & instv.immediate;
-            else
-              data.operand2<="1111111111111111" & instv.immediate;
-            end if;
-          end if;
-          control<=controlv;
-        when EX =>
-          exe_state<=MEM;
+              exe_state<=D;
+            when D=>
+              instv.PC:=PC;
+              instv.instruction:=inst_out;
+              instv.opecode:= inst_out(31 downto 26);
+              instv.rs:=inst_out(25 downto 21);
+              instv.rt:=inst_out(20 downto 16);
+              instv.rd:=inst_out(15 downto 11);
+              instv.shamt:=inst_out(10 downto 6);
+              instv.funct:=inst_out(5 downto 0);
+              instv.immediate:=inst_out(15 downto 0);
+              instv.addr:=inst_out(25 downto 0); 
+              exe_state<=EX;
+              if inst_out=x"FFFFFFFF" then
+--               core_state<=HALTED;
+                core_state<=INIT;
+                loader_reset<='1';
+                exe_state<=F;
+              end if;
+              controlv:=make_control(instv.opecode,instv.funct);
+              if controlv.RegDst='0' then
+                instv.reg_dest:=instv.rd;
+              else
+                instv.reg_dest:=instv.rt;
+              end if;
+              inst<=instv;
+              data.operand1<=reg_file(CONV_INTEGER(instv.rs));
+              if controlv.ALUSrc='0' then
+                data.operand2<=reg_file(CONV_INTEGER(instv.rt));
+              else
+                if instv.immediate(15) ='0' then
+                  data.operand2<="0000000000000000" & instv.immediate;
+                else
+                  data.operand2<="1111111111111111" & instv.immediate;
+                end if;
+              end if;
+              control<=controlv;
+            when EX =>
+              exe_state<=MEM;
 
 --          operand1<=data.operand1;
 --          operand2<=data.operand2;
 --          alu_control<=make_alu_control(inst.opecode,inst.funct);
 --          shamt<=inst.shamt;
-          data.result<=result;
-
-          case (control.PC_control) is
-            when normal =>
-              data.newPC<=PC+1;
-            when b=>
-              if inst.immediate(15)='0' then
-                report "PLUS";
-                data.newPC<=PC+1+("0000000000000000" & inst.immediate);
-              else
-                report "MINUS";
-                data.newPC<=PC+1+("1111111111111111" & inst.immediate);
-              end if;
-            when j=>
-              data.newPC<="0000000000000000"&inst.immediate;
-            when jr =>
-              data.newPC<=reg_file(CONV_INTEGER(inst.rs));
-          end case;
-
+              data.result<=result;
+              case (control.PC_control) is
+                when normal =>
+                  data.newPC<=PC+1;
+                when b=>
+                  if inst.immediate(15)='0' then
+                    report "PLUS";
+                    data.newPC<=PC+1+("0000000000000000" & inst.immediate);
+                  else
+                    report "MINUS";
+                    data.newPC<=PC+1+("1111111111111111" & inst.immediate);
+                  end if;
+                when j=>
+                  data.newPC<="0000000000000000"&inst.immediate;
+                when jr =>
+                  data.newPC<=reg_file(CONV_INTEGER(inst.rs));
+              end case;
           --memaddr
-          inst.memaddr<=data.operand1(19 downto 0)+sign_extension(inst.immediate);
-          
-        when MEM =>
-          control.iszero<=iszero;
-          if control.IORead='1' then
-			   if IO_empty='0'then
-					data.result<=IO_recv_data;
-					IO_re_cpu<='1';
-					exe_state<=WB;
-				end if;
-          elsif control.IOWrite='1' then
-            if IO_full='0' then
-              IO_we<='1';
-              IO_send_data<=data.operand2;
-              exe_state<=WB;
-            end if;
-          elsif control.Memwrite='1' then
-            case (memory_count) is
-              when "000" =>
-                SRAM_ADDR<=inst.memaddr;
-                SRAM_WE<='1';
-                SRAM_DATA<=reg_file(CONV_INTEGER(inst.rt));
-                memory_count<=memory_count+"001";
-              when others=>
-                SRAM_WE<='0';
-                SRAM_DATA<="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
-                memory_count<="000";
+              inst.memaddr<=data.operand1(19 downto 0)+sign_extension(inst.immediate);
+            when MEM =>
+              control.iszero<=iszero;
+              if control.IORead='1' then
+                if IO_empty='0'then
+                  data.result<=IO_recv_data;
+                  IO_re_cpu<='1';
+                  exe_state<=WB;
+                end if;
+              elsif control.IOWrite='1' then
+                if IO_full='0' then
+                  IO_we<='1';
+                  IO_send_data<=data.operand2;
+                  exe_state<=WB;
+                end if;
+              elsif control.Memwrite='1' then
+                case (memory_count) is
+                  when "000" =>
+                    SRAM_ADDR<=inst.memaddr;
+                    SRAM_WE<='1';
+                    SRAM_DATA<=reg_file(CONV_INTEGER(inst.rt));
+                    memory_count<=memory_count+"001";
+                  when others=>
+                    SRAM_WE<='0';
+                    SRAM_DATA<="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+                    memory_count<="000";
+                    exe_state<=WB;
+                end case;
+              elsif control.MemRead='1' then
+                case (memory_count) is
+                  when "000" =>
+                    SRAM_ADDR<=inst.memaddr;
+                    SRAM_DATA<=reg_file(CONV_INTEGER(inst.rt));
+                    memory_count<=memory_count+"001";
+                  when memory_wait =>
+                    memory_count<="000";
+                    data.result<=SRAM_DATA;
+                    exe_state<=WB;
+                  when others=>
+                    memory_count<=memory_count+"001";
+                end case;
+              else
                 exe_state<=WB;
-            end case;
-          elsif control.MemRead='1' then
-            case (memory_count) is
-              when "000" =>
-                SRAM_ADDR<=inst.memaddr;
-                SRAM_DATA<=reg_file(CONV_INTEGER(inst.rt));
-                memory_count<=memory_count+"001";
-              when memory_wait =>
-                memory_count<="000";
-                data.result<=SRAM_DATA;
-                exe_state<=WB;
-              when others=>
-                memory_count<=memory_count+"001";
-            end case;
-          else
-			    exe_state<=WB;
-			 end if;
-        when WB =>
-          IO_re_cpu<='0';
-         IO_we<='0';
-          
-           if control.RegWrite='1' then
-             if inst.reg_dest /= x"00000" then
-               reg_file(CONV_INTEGER(inst.reg_dest))<=data.result;
-             end if;
-           end if;
-          exe_state<=F;
-           if inst.opecode=x"03" then  --jump and link
-             reg_file(31)<=PC+x"00000001";
-           end if;
-           case (control.PC_control) is
-             when normal =>
-               vPC:=PC+x"00000001";
-             when j| jr =>
-               vPC:=data.newPC;
-             when b =>
-               if (inst.opecode="000100" and control.isZero='1') or
-                  (inst.opecode="000101" and control.isZero='0') then
-                 vPC:=data.newPC;
-               else
-                 vPC:=PC+x"00000001";
-               end if;
-           end case;
-           PC<=vPC;
-      end case;
-    when HALTED =>
+              end if;
+            when WB =>
+              IO_re_cpu<='0';
+              IO_we<='0';
+              if control.RegWrite='1' then
+                if inst.reg_dest /= x"00000" then
+                  reg_file(CONV_INTEGER(inst.reg_dest))<=data.result;
+                end if;
+              end if;
+              exe_state<=F;
+              if inst.opecode=x"03" then  --jump and link
+                reg_file(31)<=PC+x"00000001";
+              end if;
+              case (control.PC_control) is
+                when normal =>
+                  vPC:=PC+x"00000001";
+                when j| jr =>
+                  vPC:=data.newPC;
+                when b =>
+                  if (inst.opecode="000100" and control.isZero='1') or
+                    (inst.opecode="000101" and control.isZero='0') then
+                    vPC:=data.newPC;
+                  else
+                    vPC:=PC+x"00000001";
+                  end if;
+              end case;
+              PC<=vPC;
+          end case;
+        when HALTED =>
       -- do nothing;
-  end case;
-  end if;
+      end case;
+    end if;
   end process;
-  
+
   bram_info:process(inst_addr,inst_in,inst_we,inst_out)
   begin
     if rising_edge(clk) then
