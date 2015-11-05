efdcd03ae77a4fe6fc2c49d5edb9d5f0324a5aea
Mod4 <musesc.ll@gmail.com>
2015-11-06 02:20:35 +0900

    finv : modified table generator

--- fpu/finv/extable.c
+++ fpu/finv/extable.c
@@ -14,42 +14,100 @@
 // ...
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
 
-int main(int argc, char *argv[]){
-	uint64_t a; // a0(10bit)
-	uint64_t i; // initial
-	uint64_t c;
-	uint64_t g; // gradient
+// to <= from(up downto down);
+union hoge{
+	float    f;
+	uint32_t i;
+};
 
-	int j;
+uint32_t fromdownto(uint32_t from, int up, int down){
+	uint32_t to = 0;
+	int i;
 
-	printf("MEMORY_INITIALIZATION_RADIX=2;\nMEMORY_INITIALIZATION_VECTOR=\n");
-	for(a = 512; a < 1024; a++){
-		// calcrate initial number x0(24bit)
-		i = (((uint64_t)1 << 32)/a + (((uint64_t)1 << 32)/(a+(uint64_t)1)));
-		if(i & (uint64_t)1 == 1){
-			i = i >> 1 + (uint64_t)1;
-		}else{
-			i = i >> 1;
-		}
-		// calcrate constant c(24bit)
-		c = 2*i - ((((a*i) >> 9)*i) >> 23);
-
-		// calcrate gradient g(8bit)
-		g = (i*i) >> 40;
-
-		for(j = 0; j < 24; j++){
-			printf("%d", (int)((c & ((uint64_t)1 << (23-j))) >> (23-j)));
-		}
-		for(j = 0; j < 8; j++){
-			printf("%d", (int)((g & ((uint64_t)1 << (7-j))) >> (7-j)));
-		}
-		if(a == 1023){
-			puts(";");
-		}else{
-			puts(",");
-		}
+	for(i = up; i > down-1; i--){
+		to = (to << 1) | ((from >> i) & (uint32_t)1);
 	}
 
+	return to;
+}
+
+void printbit(char *name, uint32_t f, int up, int down){
+	int i,j;
+//	printf("%s =",name);
+	j = down;
+
+	for(i = 0; down < up+1; down++){
+		if(up == 31 && j == 0){
+			if(i == 0 || i == 1 || i == 9) printf(" ");
+			i++;
+		}
+		printf("%d", (int)((f & ((uint32_t)1 << (up-down))) >> (up-down)));
+	}
+//	puts("");
+}
+
+// to(up downto down) <= from;
+uint32_t todownto(uint32_t from, uint32_t to, int up, int down){
+	uint32_t mask = 0; // 代入すべき箇所を0で初期化
+	int i;
+	for(i = 0; i < down; i++){
+		mask = mask | ((uint32_t)1 << i);
+	}
+	for(i = up+1; i < 32; i++){
+		mask = mask | ((uint32_t)1 << i);
+	}
+	to = to & mask;
+	for(i = down; i < up+1; i++){
+		to = to | ((from & (uint32_t)1) << i);
+		from = from >> 1;
+	}
+	return to;
+}	 
+
+int main(int argc, char *argv[]){
+	union hoge input;
+	union hoge answer;
+	union hoge a0;
+	union hoge x0; // initial number
+	union hoge x1; // to derive initial number
+	union hoge x2; // to derive initial number
+	union hoge constant;
+	union hoge gradient;
+	union hoge one;
+	uint32_t key;
+
+	uint32_t a1; // 13bit
+	uint32_t gr; // 9bit gradient(+hidden bit)
+	union hoge out;
+
+	input.f  = 1.0;
+	one.f    = 1.0;
+
+  puts("MEMORY_INITIALIZATION_RADIX=2;\nMEMORY_INITIALIZATION_VECTOR=");
+	for(key = 0; key < 0x400; key++){
+		input.i = todownto(key, input.i, 22, 13);
+		answer.f = one.f / input.f;
+		a0.i = fromdownto(input.i, 31, 13) << 13;
+
+		x1.f = one.f / a0.f;
+		x2.i = a0.i + 0x2000;
+		x2.f = one.f / x2.f;
+		x0.f = (x1.f + x2.f)/2;
+
+		constant.f = 2*x0.f - a0.f * x0.f * x0.f;
+		gradient.f = x0.f * x0.f;
+
+		gr = fromdownto(gradient.i, 22, 14) + 0x200;
+		out.i = constant.i + ((a1 * gr) >> 10);
+
+		printbit("output  ", out.i, 22, 0);
+		printbit("gradient", gr   ,  8, 0);
+		if(key == 0x3ff){
+		  printf(";");
+		}else{
+		  puts(",");
+		}
+	}
 
 	return 0;
 }
--- fpu/finv/table.c
+++ fpu/finv/table.c
@@ -14,51 +14,99 @@
 // ...
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
 
-int main(int argc, char *argv[]){
-  uint64_t f;
-	uint64_t b;
-  uint64_t a; // a0(10bit)
-	uint64_t i; // initial
-	uint64_t c;
-	uint64_t g; // gradient
-	uint64_t ans;
-	uint64_t o;
+// to <= from(up downto down);
+union hoge{
+  float    f;
+	uint32_t i;
+};
 
-	int j;
+uint32_t fromdownto(uint32_t from, int up, int down){
+  uint32_t to = 0;
+	int i;
 
-//  for(a = 512; a < 1024; a++){
-  f = 10658464;
-	ans = 70368744177664 / f;
-	a = 650;
-	b = 8864;
-    // calcrate initial number x0(24bit)
-    i = (((uint64_t)1 << 32)/a + (((uint64_t)1 << 32)/(a+(uint64_t)1)));
-		if((i & (uint64_t)1) == 1){
-		  i = (i >> 1) + (uint64_t)1;
-		}else{
-		  i = (i >> 1);
+	for(i = up; i > down-1; i--){
+	  to = (to << 1) | ((from >> i) & (uint32_t)1);
+	}
+
+	return to;
+}
+
+// to(up downto down) <= from;
+uint32_t todownto(uint32_t from, uint32_t to, int up, int down){
+  uint32_t mask = 0; // 代入すべき箇所を0で初期化
+  int i;
+	for(i = 0; i < mask; i++){
+	  mask = mask | ((uint32_t)1 << i);
+	}
+	for(i = up+1; i < 64; i++){
+	  mask = mask | ((uint32_t)1 << i);
+	}
+	to = to & mask;
+  for(i = down; i < up+1; i++){
+	  to = to | ((from & (uint32_t)1) << i);
+		from >> 1;
+	}
+	return to;
+}
+
+void printbit(char *name, uint32_t f, int up, int down){
+  int i,j;
+  printf("%s =",name);
+	j = down;
+
+  for(i = 0; down < up+1; down++){
+	  if(up == 31 && j == 0){
+      if(i == 0 || i == 1 || i == 9) printf(" ");
+			i++;
 		}
-		// calcrate constant c(24bit)
-		c = 2*i - ((a*i*i) >> 32);
-
-		// calcrate gradient g(8bit)
-		g = (i*i) >> 40;
-	o = c + ((b*g) >> 6);
-
-  printf("ans =");
-  for(j = 0; j < 24 ; j++){
-		if(j % 5 == 0) printf(" ");
-	  printf("%d", (int)((ans & ((uint64_t)1 << (23-j))) >> (23-j)));
+	  printf("%d", (int)((f & ((uint32_t)1 << (up-down))) >> (up-down)));
 	}
-	printf("\no   =");
-	for(j = 0; j < 24; j++){
-		if(j % 5 == 0) printf(" ");
-	  printf("%d", (int)((o   & ((uint64_t)1 << (23-j))) >> (23-j)));
-	}
-  
 	puts("");
+}	 
+
+int main(int argc, char *argv[]){
+  union hoge input;
+	union hoge answer;
+	union hoge a0;
+	union hoge x0; // initial number
+	union hoge x1; // to derive initial number
+	union hoge x2; // to derive initial number
+	union hoge constant;
+	union hoge gradient;
+	union hoge one;
+	uint32_t key;
+
+	uint32_t a1; // 13bit
+	uint32_t gr; // 9bit gradient(+hidden bit)
+	union hoge out;
+
+  input.f  = 1.1111111;
+	one.f    = 1.0;
+	answer.f = one.f / input.f;
+
+//  for(key = 0; key < 0x400; key++){
+//  x1.i = todownto(key, input.i, 22, 13);
+//	x2.i = todownto(key, input.i, 22, 13) + 0x2000;
+  a0.i = fromdownto(input.i, 31, 13) << 13;
+	a1 = fromdownto(input.i, 12,  0);
+
+	x1.f = one.f / a0.f;
+	x2.i = a0.i + 0x2000;
+	x2.f = one.f / x2.f;
+	x0.f = (x1.f + x2.f)/2;
+
+  constant.f = 2*x0.f - a0.f * x0.f * x0.f;
+	gradient.f = x0.f * x0.f;
+
+	gr = fromdownto(gradient.i, 22, 14) + 0x200;
+	out.i = constant.i + ((a1 * gr) >> 10);
+    
 //	}
-
-
+  printbit("input   ", input.i, 31, 0);
+  printbit("a1      ", a1, 31, 0);
+  printbit("constant", constant.i, 31, 0);
+	printbit("answer  ", answer.i, 31, 0);
+	printbit("output  ", out.i, 31, 0);
+  
 	return 0;
 }
