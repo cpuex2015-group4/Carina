9e0cff3ef0d8c2a9ef8194f4745b9619c637098d
Yuki Imai <yuki08281993@gmail.com>
2015-11-06 18:08:34 +0900

    cpu_file_tb_added.

--- core/cpu_file_tb.vhd
+++ core/cpu_file_tb.vhd
@@ -29,6 +29,8 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 use ieee.std_logic_unsigned.all; 
 use ieee.std_logic_arith.all;
+
+
 library work;
 use work.p_type.all;
 -- Uncomment the following library declaration if using
@@ -51,6 +53,7 @@ ARCHITECTURE behavior OF cpu_tb IS
          IO_WE : OUT  std_logic;
          IO_RE : OUT  std_logic;
          IO_send_data : OUT  std_logic_vector(31 downto 0);
+         word_access:out std_logic;
            --SRAM
   SRAM_ADDR:out std_logic_vector(19 downto 0);
   SRAM_DATA:inout datat;
@@ -86,21 +89,27 @@ end component;
    signal IO_RE : std_logic;
    signal IO_send_data : std_logic_vector(31 downto 0);
    signal DEBUG : top_debug_out;
-
+signal word_access :std_logic;
    -- Clock period definitions
    constant clk_period : time := 15 ns;
-   
-    constant ROMMAX:Integer:=15;
-       type rom_t is array (0 to ROMMAX) of std_logic_vector(31 downto 0);
-    constant rom:rom_t:=(
-	  conv_std_logic_vector(0,32),
-	  conv_std_logic_vector(12,32),
-  conv_std_logic_vector(0,32),
-	  conv_std_logic_vector(5,32),
-    x"6c080000",X"6c080000",x"6c080000",x"6c080000",x"6c080000",
-      x"2001000A", --kouhan is the number);
-      x"20020000",x"00221020",x"2021FFFF",x"1420FFFD",x"6c020000",x"FFFFFFFF"
-  );
+
+
+--FILEIO
+   type    BIN is file of character;
+    file    FILEPOINT   :   BIN open READ_MODE is "loader_data.o";
+
+
+--    constant ROMMAX:Integer:=15;
+--       type rom_t is array (0 to ROMMAX) of std_logic_vector(31 downto 0);
+--    constant rom:rom_t:=(
+--	  conv_std_logic_vector(0,32),
+--	  conv_std_logic_vector(12,32),
+  	--  conv_std_logic_vector(0,32),
+--	  conv_std_logic_vector(5,32),
+--    x"6c080000",X"6c080000",x"6c080000",x"6c080000",x"6c080000",
+--      x"2001000A", --kouhan is the number);
+--      x"20020000",x"00221020",x"2021FFFF",x"1420FFFD",x"6c020000",x"FFFFFFFF"
+--  );
 
 
 --        constant ROMMAX:Integer:=14;
@@ -113,14 +122,14 @@ end component;
 --    x"20040050",x"0c000009",x"2004004f",x"0c000009",x"20040048",x"0c000009",x"20040045",x"0c000009",x"ffffffff",x"6c040000",x"03e00008"  );    
 
     
---        constant ROMMAX:Integer:=9;
---       type rom_t is array (0 to ROMMAX) of std_logic_vector(31 downto 0);
---    constant rom:rom_t:=(
---	  conv_std_logic_vector(0,32),
---	  conv_std_logic_vector(3,32),
---  	  conv_std_logic_vector(3,32),
---	  conv_std_logic_vector(0,32),
---      x"8c0500aa",x"6c050000",x"ffffffff",x"cafecafe",x"deaddead",x"beefbeef");    
+        constant ROMMAX:Integer:=9;
+       type rom_t is array (0 to ROMMAX) of std_logic_vector(31 downto 0);
+    constant rom:rom_t:=(
+	  conv_std_logic_vector(0,32),
+	  conv_std_logic_vector(3,32),
+  	  conv_std_logic_vector(3,32),
+	  conv_std_logic_vector(0,32),
+      x"8c0500aa",x"6c050000",x"ffffffff",x"cafecafe",x"deaddead",x"beefbeef");    
 
 
 BEGIN
@@ -134,6 +143,7 @@ BEGIN
           IO_WE => IO_WE,
           IO_RE => IO_RE,
           IO_send_data => IO_send_data,
+          word_access=>word_access,
 
   --SRAM
           SRAM_ADDR=>SRAM_ADDR,
@@ -177,28 +187,38 @@ port map (
    end process;
    -- Stimulus process
    stim_proc: process
-   begin		
+     variable FREAD_CHAR:character;
+     variable data:datat:=x"00000000";
+   begin
       -- hold reset state for 100 ns.
-      wait for 100 ns;	
+      wait for 100 ns;
 
       wait for clk_period*10;
 
       -- insert stimulus here
 		--wait until rising_edge(clk);
-		
-		io_empty<='0';
-
-      for I in 0 to ROMMAX loop
-			 io_recv_data<=rom(I);
-			 io_empty<='0';
-			 if io_re='0' then
-			   wait until  io_re='1';
-			 end if;
-			 io_empty<='1';
-			 wait for clk_period*10;
-			 
+      while(endfile(FILEPOINT)=false) loop
+        if word_access='1' then
+          read( FILEPOINT, FREAD_CHAR );
+          data(31 downto 24):=(conv_std_logic_vector(CHARacter'pos(FREAD_CHAR),8));
+          read( FILEPOINT, FREAD_CHAR );
+          data(23 downto 16):=(conv_std_logic_vector(CHARacter'pos(FREAD_CHAR),8));
+          read( FILEPOINT, FREAD_CHAR );
+          data(15 downto 8):=(conv_std_logic_vector(CHARacter'pos(FREAD_CHAR),8));
+          read( FILEPOINT, FREAD_CHAR );
+          data(7 downto 0):=(conv_std_logic_vector(CHARacter'pos(FREAD_CHAR),8));
+        else
+          read( FILEPOINT, FREAD_CHAR );
+          data:=x"000000" &(conv_std_logic_vector(CHARacter'pos(FREAD_CHAR),8));
+        end if;
+        io_empty<='0';
+        io_recv_data<=data;
+        if io_re='0' then
+          wait until  io_re='1';
+        end if;
+        io_empty<='1';
+        wait for clk_period*100;
       end loop;
-      wait;
    end process;
 
-END;
+end;
