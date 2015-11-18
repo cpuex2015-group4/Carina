4b3fde9267502110201c337b5d56d08e29148686
levelfour <levelfourslv@gmail.com>
2015-11-18 13:33:34 +0900

    add fabs,floor,sqrt plot

--- tools/plot.py
+++ tools/plot.py
@@ -21,7 +21,7 @@ def int_of_float(xran):
 		with open(TMP_ML + ".ml", "w") as f:
 			f.write("int_of_float({:f})".format(x))
 		compile(TMP_ML, quiet = True)
-		sys.stdout.write("{0}8D{0}K{1:-8}".format("\033[", x))
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
 		Y.append(csim(TMP_ML)[0])
 	plt.plot(X, Y)
 	plt.savefig("int_of_float.png")
@@ -33,7 +33,7 @@ def float_of_int(xran):
 		with open(TMP_ML + ".ml", "w") as f:
 			f.write("float_of_int({})".format(int(x)))
 		compile(TMP_ML, quiet = True)
-		sys.stdout.write("{0}8D{0}K{1:-8}".format("\033[", x))
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
 		Y.append(csim(TMP_ML)[1])
 	plt.plot(X, Y)
 	plt.savefig("float_of_int.png")
@@ -45,7 +45,7 @@ def fcos(xran):
 		with open(TMP_ML + ".ml", "w") as f:
 			f.write("cos({:f})".format(x))
 		compile(TMP_ML, quiet = True)
-		sys.stdout.write("{0}8D{0}K{1:-8}".format("\033[", x))
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
 		Y.append(csim(TMP_ML)[1])
 	plt.plot(X, Y)
 	plt.savefig("cos.png")
@@ -57,7 +57,7 @@ def fsin(xran):
 		with open(TMP_ML + ".ml", "w") as f:
 			f.write("sin({:f})".format(x))
 		compile(TMP_ML, quiet = True)
-		sys.stdout.write("{0}8D{0}K{1:-8}".format("\033[", x))
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
 		Y.append(csim(TMP_ML)[1])
 	plt.plot(X, Y)
 	plt.savefig("sin.png")
@@ -69,10 +69,46 @@ def fatan(xran):
 		with open(TMP_ML + ".ml", "w") as f:
 			f.write("atan({:f})".format(x))
 		compile(TMP_ML, quiet = True)
-		sys.stdout.write("{0}8D{0}K{1:-8}".format("\033[", x))
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
 		Y.append(csim(TMP_ML)[1])
 	plt.plot(X, Y)
 	plt.savefig("atan.png")
+
+def fabs(xran):
+	Y = []
+	sys.stdout.write("x = ")
+	for x in xran:
+		with open(TMP_ML + ".ml", "w") as f:
+			f.write("fabs({:f})".format(x))
+		compile(TMP_ML, quiet = True)
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
+		Y.append(csim(TMP_ML)[1])
+	plt.plot(X, Y)
+	plt.savefig("fabs.png")
+
+def floor(xran):
+	Y = []
+	sys.stdout.write("x = ")
+	for x in xran:
+		with open(TMP_ML + ".ml", "w") as f:
+			f.write("floor({:f})".format(x))
+		compile(TMP_ML, quiet = True)
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
+		Y.append(csim(TMP_ML)[1])
+	plt.plot(X, Y)
+	plt.savefig("floor.png")
+
+def sqrt(xran):
+	Y = []
+	sys.stdout.write("x = ")
+	for x in xran:
+		with open(TMP_ML + ".ml", "w") as f:
+			f.write("sqrt({:f})".format(x))
+		compile(TMP_ML, quiet = True)
+		sys.stdout.write("{0}20D{0}K{1:-8}".format("\033[", x))
+		Y.append(csim(TMP_ML)[1])
+	plt.plot(X, Y)
+	plt.savefig("sqrt.png")
 
 if __name__ == "__main__":
 	X = np.arange(-10, 10, 0.1)
@@ -88,4 +124,12 @@ if __name__ == "__main__":
 		fsin(X)
 	elif spec == "atan":
 		fatan(X)
+	elif spec == "fabs":
+		fabs(X)
+	elif spec == "floor":
+		floor(X)
+	elif spec == "sqrt":
+		sqrt(X)
+	else:
+		assert "invalid spec"
 	clean()
