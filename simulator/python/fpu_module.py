f377023fb098711a4d5890e5cfbba34851d68a42
levelfour <levelfourslv@gmail.com>
2015-11-20 21:28:01 +0900

    fix finv integrator with pysim

--- simulator/python/fpu_module.py
+++ simulator/python/fpu_module.py
@@ -13,7 +13,7 @@ class FpuModule:
 	c_fop1_func_type = CFUNCTYPE(c_uint, c_uint)
 	c_fadd_func = cast(c_module.fadd, c_fop2_func_type)
 	c_fmul_func = cast(c_module.fmul, c_fop2_func_type)
-	c_fint_func = cast(c_module.fmul, c_fop1_func_type)
+	c_finv_func = cast(c_module.finv, c_fop1_func_type)
 
 	@classmethod
 	def fadd(cls, f1, f2):
