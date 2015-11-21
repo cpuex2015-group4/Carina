2ebc01396169242d9bcabdc5e83aaa089d14a4a8
Yuki Imai <yuki08281993@gmail.com>
2015-11-21 23:38:58 +0900

    file-seiri

--- coretests/invtest.ml
+++
@@ -1,5 +0,0 @@
-let rec dbl f = f +. f in
-let cr = dbl (float_of_int 270) /. 400.0 -. 1.5 in
-let ci = dbl (float_of_int 46) /. 400.0 -. 1.0 in
-print_int(int_of_float cr);
-print_int(int_of_float ci);
