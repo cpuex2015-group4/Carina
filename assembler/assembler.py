9235b50e8be1dc197eedb945e5b82521e9f4ddce
levelfour <levelfourslv@gmail.com>
2015-11-24 09:08:59 +0900

    mistake

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -88,7 +88,7 @@ class Assembler:
 			else:
 				offset = 0
 			data_label_dict = {k: v + len(text_lines) for k, v in data_label_dict.items()}
-			label_dict = {k: v + 4 for d in [text_label_dict, data_label_dict] for k, v in d.items()}
+			label_dict = {k: v + offset for d in [text_label_dict, data_label_dict] for k, v in d.items()}
 			
 			# label represent for heap pointer
 			label_dict["min_caml_heap_pointer"] = len(text_lines) + len(data_lines)
